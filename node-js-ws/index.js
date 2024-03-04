7216f81fee41cd29c5063f1cfe3e212b30259c62
Guilherme Duffes Marques <guilherme.duffes@al.infnet.edu.br>
2024-03-04 15:09:04 -0300

    pronto para implementações

--- node-js-ws/index.js
+++ node-js-ws/index.js
@@ -1,78 +1,68 @@
-const pup = require("puppeteer");
+// Criado por Guilherme Duffes 04/03/2024 14h30.
+
+const puppeteer = require("puppeteer");
 const express = require("express");
 const fs = require("fs");
+const cheerio = require("cheerio");
 const app = express();
 const port = 3000;
+
+const url = "https://www.mercadolivre.com.br/";
 
 app.use(express.json());
 
 app.get("/pesquisar", async (req, res) => {
   const pesquisaPor = req.query.termo;
 
+  const nomeArquivo = "resultados.csv";
   try {
     console.log(
       "Requisição de pesquisa recebida. Termo de pesquisa:",
       pesquisaPor
     );
 
-    const navegador = await pup.launch();
+    const navegador = await puppeteer.launch({ headless: false });
     const pagina = await navegador.newPage();
-    await pagina.goto(`https://www.mercadolivre.com.br/`);
-    console.log("Página inicial carregada.");
-    await pagina.waitForSelector("#cb1-edit");
-    console.log("Campo de pesquisa encontrado.");
-    await pagina.type("#cb1-edit", pesquisaPor);
-    console.log("Termo de pesquisa inserido:", pesquisaPor);
+    await pagina.goto(url);
+
+    await pagina.waitForSelector(".nav-search-input");
+    await pagina.type(".nav-search-input", pesquisaPor);
     await Promise.all([
       pagina.waitForNavigation(),
       pagina.click(".nav-search-btn"),
     ]);
-    console.log("Pesquisa realizada.");
-    await pagina.waitForSelector(".ui-search-result__content");
-    console.log("Resultados da pesquisa carregados.");
+    await pagina.waitForSelector(".ui-search-layout__item");
 
-    let dadosCSV = "Título do Produto,Preço do Produto\n";
+    const html = await pagina.content();
+    const $ = cheerio.load(html);
 
-    const resultados = await pagina.$$(".ui-search-result__content");
-
-    for (const resultado of resultados) {
-      const linkProduto = await resultado.$eval(
-        "a.ui-search-link",
-        (element) => element.href
-      );
-      console.log("Link do produto:", linkProduto);
-      await pagina.goto(linkProduto);
-      console.log("Página do produto carregada.");
-      await pagina.waitForSelector("h1.ui-pdp-title");
-      console.log("Título do produto encontrado.");
-
-      const tituloProduto = await pagina.$eval("h1.ui-pdp-title", (element) =>
-        element.textContent.trim().toUpperCase()
-      );
-      console.log("Título do produto:", tituloProduto);
-
-      let precoProduto = "Preço não encontrado";
-      const precoElement = await pagina.waitForSelector("[data-testid='price-part']", { timeout: 5000 }); // Aumentar o tempo de espera para o elemento de preço
-      if (precoElement) {
-        precoProduto = await precoElement.evaluate((element) =>
-          element.textContent.trim()
-        );
-      }
-      console.log("Preço do produto:", precoProduto);
-
-      dadosCSV += `"${tituloProduto}","${precoProduto}"\n`;
-    }
+    const dadosCSV = await extrairDadosProdutos($);
 
     await navegador.close();
 
-    fs.writeFileSync("resultados.csv", dadosCSV, "utf-8");
+    fs.writeFileSync(nomeArquivo, dadosCSV, "utf-8");
 
+    console.log("Dados salvos em", nomeArquivo);
     res.send({ message: "Pesquisa concluída com sucesso!" });
   } catch (error) {
     console.error("Erro ao realizar a pesquisa:", error);
     res.status(500).send({ error: "Ocorreu um erro ao realizar a pesquisa." });
   }
 });
+
+async function extrairDadosProdutos($) {
+  let dadosCSV = "Título do Produto,Preço do Produto\n";
+
+  $(".ui-search-layout__item").each((i, elemento) => {
+    // const linkProduto = $(elemento).find(".ui-search-link").attr("href");
+    const tituloProduto = $(elemento).find(".ui-search-item__title").text().trim().toUpperCase();
+    const precoProduto = $(elemento).find("span.andes-money-amount.ui-search-price__part.ui-search-price__part--medium.andes-money-amount--cents-superscript").text().trim();
+
+    dadosCSV += `"${tituloProduto}","${precoProduto}"\n`;
+  });
+
+  return dadosCSV;
+}
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
